cf73f6c0cd9bfcd71e00cf8c6a53e3d9461d95c2
张雪松 <zhangxuesong219@gmail.com>
2020-11-14 12:08:54 +0800

    backup

--- stabledollar/test/Masset.test.js
+++ stabledollar/test/Masset.test.js
@@ -1,48 +1,59 @@
 const {expectRevert, time} = require('@openzeppelin/test-helpers');
+const basketMgr = artifacts.require('BasketManager.sol');
+const forgeValidator = artifacts.require('ForgeValidator.sol');
 const mAsset = artifacts.require('Masset.sol');
+const erc20 = artifacts.require('Token');
+const mock_aave = artifacts.require('MockAAVE');
 
-contract('mAsset', ([alice, bob, carol, dev, minter]) => {
+contract('Masset - Mint', ([alice, bob, carol, dev, minter]) => {
 
     beforeEach(async () => {
-        this.ctl = await mAsset.new(alice, {from: alice});
+        // mock erc20
+        this.erc20 = await erc20.new('DAI', 'DAI', { from: alice });
+        // deploy ccontracts
+        this.basketMgr = await basketMgr.new();
+        this.forgeValidator = await forgeValidator.new();
+        this.xsdt = await mAsset.new("XSDT", "XSDT", { from: alice });
+
+        // deploy mock platform
+        this.aave = await mock_aave.new();
+
+        // console.log("before init", await this.xsdt.forgeValidator())
+        // initialize mAsset
+        await this.xsdt.initialize(this.basketMgr.address, this.xsdt.address);
+        // console.log("after init", await this.xsdt.forgeValidator())
     });
 
     it('Mint', async () => {
-        // const version = await this.ctl.getVersion();
-        // console.log('version:', version);
 
-        const amount = await this.ctl.mint(bob, 2, {from: alice});
-        console.log('amount: ', amount);
-        // const random1 = await this.ctl.genRandNumber();
-        // console.log(random1);
+        console.log("before balance", (await this.erc20.balanceOf(alice)).toString())
+        // mint token to user.
+        await this.erc20.mint(alice, 100000);
+        console.log("after balance", (await this.erc20.balanceOf(alice)).toString())
 
-        // assert.equal(devAddress.valueOf(), alice);
-        // assert.equal(currentVersion.valueOf(), '0');
+        // approve to mAsset contract
+        await this.erc20.approve(this.xsdt.address, 100000);
 
-        // 0.02, 0.05
-        // await this.ctl.startVersion('20000000000000000', '50000000000000000', '20', '1024', {from: alice});
-        // const currentVersion2 = await this.ctl.currentVersion();
-        // console.log('currentVersion2:', currentVersion2.toString())
+        // set platform config of `basketMgr`
+        console.log('before set', await this.basketMgr.mAsset())
+        await this.basketMgr.initialize(this.xsdt.address, [this.erc20.address], [this.aave.address], [10**8], [false]);
+        console.log('after set', await this.basketMgr.mAsset())
 
-        // const allVersion = await this.ctl.allVersion('1')
-        // console.log('allVersion.timeOutBlockSize:', allVersion.timeOutBlockSize.toString());
-        // console.log('allVersion.basePrice:', allVersion.basePrice.toString());
+        // xsdt contract approve platform: aavev
+        this.xsdt.approve(this.aave.address, 10000000)
+        // BUG!!!
+        this.xsdt.approve(bob, 10000000, { from: bob })
 
-        // const gridInfo = await this.ctl.getGridInfo('19')
-        // console.log('gridInfo:', gridInfo[1].toString())
+        console.log('before mint', (await this.xsdt.balanceOf(alice)).toString())
+        await this.xsdt.mint(this.erc20.address, 100)
+        console.log('after mint', (await this.xsdt.balanceOf(alice)).toString())
 
-        // 1.4 1400000000000000000
-        // const level = await this.ctl.getLevelByPrice('1400000000000000000');
-        // // assert.equal(level.toString(), '0');
-        // console.log('level:', level.toString())
+        console.log("before redeem, underlying balance:", (await this.erc20.balanceOf(alice)).toString())
+        // await debug(this.xsdt.redeem(this.erc20.address, 6))
+        await this.xsdt.redeem(this.erc20.address, 6)
+        console.log("after redeem, underlying balance:", (await this.erc20.balanceOf(alice)).toString())
+        console.log('after redeem', (await this.xsdt.balanceOf(alice)).toString())
 
-        // const poolTotalFee = await this.ctl.poolTotalFee();
-        // assert.equal(level.toString(), '0');
-        // console.log('poolTotalFee:', poolTotalFee.toString())
     });
-
-    // xit('TestCase_2', async () => {
-
-    // });
 
 });
